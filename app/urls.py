e4dce424ff1e2100ee191eb903cc85d0e753bd2e
cavaliersant <linodefr@gmail.com>
2025-02-03 18:06:43 -0300

    Configurações inicias, cruds, forms e templates, finalizados

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', include('brands.urls')),
+    path('', include('categories.urls')),
+    path('', include('suppliers.urls')),
+    path('', include('inflows.urls')),
+    path('', include('outflows.urls')),
+    path('', include('products.urls')),
+]
